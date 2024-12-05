783d8e4e619a44ab83cbb0c8c926eb2f39290270
KSaid-1 <uqksaida@uq.edu.au>
2024-12-05 16:57:43 +1000

    RandomForest and extratree

--- mnist/Classification/src/model_dispatcher.py
+++ mnist/Classification/src/model_dispatcher.py
@@ -9,6 +9,7 @@ models = (
     {
         "decision_tree_gini": tree.DecisionTreeClassifier(criterion='gini'),
         "decision_tree_entropy": tree.DecisionTreeClassifier(criterion='entropy'),
+        "et": ensemble.ExtraTreesClassifier(),
         "rf": ensemble.RandomForestClassifier(),
     }
 )
